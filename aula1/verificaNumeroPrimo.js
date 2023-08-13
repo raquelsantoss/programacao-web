ea85199270000ad687ee8d3e98d6a3728f714319
Raquel Santos <raquel.pri@hotmail.com>
2023-08-12 21:03:05 -0300

    fix: indentation

--- aula1/verificaNumeroPrimo.js
+++ aula1/verificaNumeroPrimo.js
@@ -1,42 +1,42 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 function isPrime(number) {
-    if (number <= 1) {
-        return false;
-    }
+  if (number <= 1) {
+    return false;
+  }
 
-    if (number <= 3) {
-        return true;
-    }
-
-    if (number % 2 === 0 || number % 3 === 0) {
-        return false;
-    }
-
-    for (let i = 5; i * i <= number; i += 6) {
-        if (number % i === 0 || number % (i + 2) === 0) {
-            return false;
-        }
-    }
-
+  if (number <= 3) {
     return true;
+  }
+
+  if (number % 2 === 0 || number % 3 === 0) {
+    return false;
+  }
+
+  for (let i = 5; i * i <= number; i += 6) {
+    if (number % i === 0 || number % (i + 2) === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 function printPrimeStatus(number) {
-    if (isPrime(number)) {
-        console.log("1 - O número é primo.");
-    } else {
-        console.log("0 - O número não é primo.");
-    }
+  if (isPrime(number)) {
+    console.log("1 - O número é primo.");
+  } else {
+    console.log("0 - O número não é primo.");
+  }
 }
 
-rl.question("Digite um número: ", function(inputNumber) {
-    const number = parseInt(inputNumber);
-    printPrimeStatus(number);
-    rl.close();
+rl.question("Digite um número: ", function (inputNumber) {
+  const number = parseInt(inputNumber);
+  printPrimeStatus(number);
+  rl.close();
 });
